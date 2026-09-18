147b5c1ffedd518c17f697a3aad11c7b39afca3a
Lina Krüger <schichten_feature_6x@icloud.com>
2026-09-18 16:58:09 +0200

    add edit button with custom colors

--- src/app/song-list/song-list.ts
+++ src/app/song-list/song-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-song-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './song-list.html',
   styleUrl: './song-list.css'
 })
